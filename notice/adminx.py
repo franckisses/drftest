cc824f4c4ee829484bdbbc916e243ea2327b8a29
franck <g-gongyan@tedu.cn>
2019-11-17 01:16:40 +0800

    test

--- notice/adminx.py
+++ notice/adminx.py
@@ -2,7 +2,6 @@ import xadmin
 from .models import CompanyInfo
 
 class CompanyInfoAdmin(object):
-    import_excel = True
     list_display = ['company','email','start_time','end_time','is_alive']
     import_excel = True
 
